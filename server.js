03dc40e2dce2be1b1019e0fb74fdf6efdd0d872b
champivoid <mgdigitalfrance@gmail.com>
2017-02-25 18:46:59 +0100

    ajout des fichiers et dossier

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+console.log('Server is running');
+
+// socket.io
+var socket = require('socket.io');
+var io = socket(server);
+
+io.on('connection', connectionMessage);
+
+
+function connectionMessage(socket) {
+	console.log('New client is connected' + socket.id);
+
+	socket.on('mouse', mouseMsg);
+
+	function mouseMsg(data) {
+		socket.broadcast.emit('mouse', data);
+		console.log(data);
+	}
+}
